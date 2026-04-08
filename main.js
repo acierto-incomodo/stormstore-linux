a31cbda0eea3d3a5cbb95bc99fec88be112819d7
acierto-incomodo <97026099+acierto-incomodo@users.noreply.github.com>
2026-04-08 17:39:14 +0200

    Act 16

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 900,
     height: 600,
+  icon: path.join(__dirname, 'icons', 'stormstore-linux.png'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
